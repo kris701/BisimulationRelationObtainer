0af72f0cbf64672ef99c6510cace58a31b3ad2f1
Kristian Skov <kris701kj@gmail.com>
2023-04-11 11:12:18 +0200

    Fixed tests

--- BisimulationRelationObtainerTests/IObtainerFunctionalityTests.cs
+++ BisimulationRelationObtainerTests/IObtainerFunctionalityTests.cs
@@ -9,69 +9,103 @@ namespace BisimulationRelationObtainerTests
         public static IEnumerable<object[]> GetTestSets_Naive()
         {
             yield return new object[] {
-                new DFAProcess("Examples/Example1/s.txt"),
-                new DFAProcess("Examples/Example1/t.txt"),
+                new DFAProcess("Examples/Example1/s.dfa"),
+                new DFAProcess("Examples/Example1/t.dfa"),
                 "[s0 [IsInit]] [t0 [IsInit]][s1] [t1][ø] [ø][s0s2 [IsFinal]] [t1t2 [IsFinal]][s1s2 [IsFinal]] [t1t2 [IsFinal]][s2 [IsFinal]] [t0t2 [IsFinal]][s0s2 [IsFinal]] [t0t2 [IsFinal]][s2 [IsFinal]] [t1t2 [IsFinal]][s2 [IsFinal]] [t2 [IsFinal]]"
             };
             yield return new object[] {
-                new DFAProcess("Examples/Example1/s.txt"),
-                new DFAProcess("Examples/Example1/s.txt"),
+                new DFAProcess("Examples/Example1/s.dfa"),
+                new DFAProcess("Examples/Example1/s.dfa"),
                 "[s0 [IsInit]] [s0 [IsInit]][s1] [s1][ø] [ø][s0s2 [IsFinal]] [s0s2 [IsFinal]][s1s2 [IsFinal]] [s1s2 [IsFinal]][s2 [IsFinal]] [s2 [IsFinal]]"
             };
             yield return new object[] {
-                new DFAProcess("Examples/Example1/t.txt"),
-                new DFAProcess("Examples/Example1/t.txt"),
+                new DFAProcess("Examples/Example1/t.dfa"),
+                new DFAProcess("Examples/Example1/t.dfa"),
                 "[t0 [IsInit]] [t0 [IsInit]][t1] [t1][ø] [ø][t1t2 [IsFinal]] [t1t2 [IsFinal]][t0t2 [IsFinal]] [t0t2 [IsFinal]][t2 [IsFinal]] [t2 [IsFinal]]"
             };
 
             yield return new object[] {
-                new DFAProcess("Examples/Example2/s.txt"),
-                new DFAProcess("Examples/Example2/t.txt"),
+                new DFAProcess("Examples/Example2/s.dfa"),
+                new DFAProcess("Examples/Example2/t.dfa"),
                 "[s0 [IsInit]] [t0 [IsInit]][s0s2 [IsFinal]] [t1 [IsFinal]][s1 [IsFinal]] [t1 [IsFinal]][s1s2 [IsFinal]] [t1 [IsFinal]][s2 [IsFinal]] [t1 [IsFinal]]"
             };
             yield return new object[] {
-                new DFAProcess("Examples/Example2/s.txt"),
-                new DFAProcess("Examples/Example2/s.txt"),
+                new DFAProcess("Examples/Example2/s.dfa"),
+                new DFAProcess("Examples/Example2/s.dfa"),
                 "[s0 [IsInit]] [s0 [IsInit]][s0s2 [IsFinal]] [s0s2 [IsFinal]][s1 [IsFinal]] [s1 [IsFinal]][s1s2 [IsFinal]] [s1s2 [IsFinal]][s2 [IsFinal]] [s2 [IsFinal]]"
             };
             yield return new object[] {
-                new DFAProcess("Examples/Example2/t.txt"),
-                new DFAProcess("Examples/Example2/t.txt"),
+                new DFAProcess("Examples/Example2/t.dfa"),
+                new DFAProcess("Examples/Example2/t.dfa"),
                 "[t0 [IsInit]] [t0 [IsInit]][t1 [IsFinal]] [t1 [IsFinal]]"
             };
         }
         public static IEnumerable<object[]> GetTestSets_HopcroftAndKarp()
         {
             yield return new object[] {
-                new DFAProcess("Examples/Example1/s.txt"),
-                new DFAProcess("Examples/Example1/t.txt"),
+                new DFAProcess("Examples/Example1/s.dfa"),
+                new DFAProcess("Examples/Example1/t.dfa"),
                 "[s0 [IsInit]] [t0 [IsInit]][s1] [t1][s0s2 [IsFinal]] [t1t2 [IsFinal]][s1s2 [IsFinal]] [t1t2 [IsFinal]][s2 [IsFinal]] [t0t2 [IsFinal]][s0s2 [IsFinal]] [t0t2 [IsFinal]][s2 [IsFinal]] [t2 [IsFinal]]"
             };
             yield return new object[] {
-                new DFAProcess("Examples/Example1/s.txt"),
-                new DFAProcess("Examples/Example1/s.txt"),
+                new DFAProcess("Examples/Example1/s.dfa"),
+                new DFAProcess("Examples/Example1/s.dfa"),
                 ""
             };
             yield return new object[] {
-                new DFAProcess("Examples/Example1/t.txt"),
-                new DFAProcess("Examples/Example1/t.txt"),
+                new DFAProcess("Examples/Example1/t.dfa"),
+                new DFAProcess("Examples/Example1/t.dfa"),
                 ""
             };
 
             yield return new object[] {
-                new DFAProcess("Examples/Example2/s.txt"),
-                new DFAProcess("Examples/Example2/t.txt"),
+                new DFAProcess("Examples/Example2/s.dfa"),
+                new DFAProcess("Examples/Example2/t.dfa"),
                 "[s0 [IsInit]] [t0 [IsInit]][s0s2 [IsFinal]] [t1 [IsFinal]][s1 [IsFinal]] [t1 [IsFinal]][s1s2 [IsFinal]] [t1 [IsFinal]][s2 [IsFinal]] [t1 [IsFinal]]"
             };
             yield return new object[] {
-                new DFAProcess("Examples/Example2/s.txt"),
-                new DFAProcess("Examples/Example2/s.txt"),
+                new DFAProcess("Examples/Example2/s.dfa"),
+                new DFAProcess("Examples/Example2/s.dfa"),
                 ""
             };
             yield return new object[] {
-                new DFAProcess("Examples/Example2/t.txt"),
-                new DFAProcess("Examples/Example2/t.txt"),
+                new DFAProcess("Examples/Example2/t.dfa"),
+                new DFAProcess("Examples/Example2/t.dfa"),
                 ""
+            };
+        }
+        public static IEnumerable<object[]> GetTestSets_PousAndBonchi()
+        {
+            yield return new object[] {
+                new DFAProcess("Examples/Example1/s.dfa"),
+                new DFAProcess("Examples/Example1/t.dfa"),
+                "[s0 [IsInit]] [t0 [IsInit]][s1] [t1][ø] [ø][s0s2 [IsFinal]] [t1t2 [IsFinal]][s2 [IsFinal]] [t0t2 [IsFinal]][s2 [IsFinal]] [t2 [IsFinal]]"
+            };
+            yield return new object[] {
+                new DFAProcess("Examples/Example1/s.dfa"),
+                new DFAProcess("Examples/Example1/s.dfa"),
+                "[s0 [IsInit]] [s0 [IsInit]][s1] [s1][ø] [ø][s0s2 [IsFinal]] [s0s2 [IsFinal]][s2 [IsFinal]] [s2 [IsFinal]]"
+            };
+            yield return new object[] {
+                new DFAProcess("Examples/Example1/t.dfa"),
+                new DFAProcess("Examples/Example1/t.dfa"),
+                "[t0 [IsInit]] [t0 [IsInit]][t1] [t1][ø] [ø][t1t2 [IsFinal]] [t1t2 [IsFinal]][t0t2 [IsFinal]] [t0t2 [IsFinal]][t2 [IsFinal]] [t2 [IsFinal]]"
+            };
+
+            yield return new object[] {
+                new DFAProcess("Examples/Example2/s.dfa"),
+                new DFAProcess("Examples/Example2/t.dfa"),
+                "[s0 [IsInit]] [t0 [IsInit]][s0s2 [IsFinal]] [t1 [IsFinal]][s1 [IsFinal]] [t1 [IsFinal]][s2 [IsFinal]] [t1 [IsFinal]]"
+            };
+            yield return new object[] {
+                new DFAProcess("Examples/Example2/s.dfa"),
+                new DFAProcess("Examples/Example2/s.dfa"),
+                "[s0 [IsInit]] [s0 [IsInit]][s0s2 [IsFinal]] [s0s2 [IsFinal]][s1 [IsFinal]] [s1 [IsFinal]][s2 [IsFinal]] [s2 [IsFinal]]"
+            };
+            yield return new object[] {
+                new DFAProcess("Examples/Example2/t.dfa"),
+                new DFAProcess("Examples/Example2/t.dfa"),
+                "[t0 [IsInit]] [t0 [IsInit]][t1 [IsFinal]] [t1 [IsFinal]]"
             };
         }
 
@@ -99,6 +133,23 @@ namespace BisimulationRelationObtainerTests
         {
             // ARRANGE
             IObtainer obtainer = new HopcroftAndKarp();
+
+            // ACT
+            var result = obtainer.ObtainRelation(P, Q);
+            string resultStr = "";
+            foreach (var item in result)
+                resultStr += item.ToString();
+
+            // ASSERT
+            Assert.AreEqual(expected, resultStr);
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(GetTestSets_PousAndBonchi), DynamicDataSourceType.Method)]
+        public void Can_PousAndBonchi_GiveCorrectRelation(DFAProcess P, DFAProcess Q, string expected)
+        {
+            // ARRANGE
+            IObtainer obtainer = new PousAndBonchi();
 
             // ACT
             var result = obtainer.ObtainRelation(P, Q);
